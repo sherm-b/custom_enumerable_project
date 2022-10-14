308246016523aae02e7324c8fbaec7663eb7d0ab
Jeremy <jeremy.steven.bess@gmail.com>
2022-10-14 13:55:30 -0700

    Create my_each_with_index method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,16 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    if block_given?
+      i = 0
+      for element in self
+        yield element, i
+        i += 1
+      end
+      return self
+    end
+  end
+
 end
 
 # You will first have to define my_each
