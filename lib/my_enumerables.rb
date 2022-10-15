2c0b05696a9fa32595d9ca23654419a53b6d6917
Jeremy <jeremy.steven.bess@gmail.com>
2022-10-15 09:47:09 -0700

    Return enumerator if no block given

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,16 +1,21 @@
 module Enumerable
   # Your code goes here
   def my_each_with_index
-    if block_given?
-      i = 0
-      for element in self
-        yield element, i
-        i += 1
-      end
-      return self
+    return to_enum(:my_each_with_index) unless block_given?
+
+    i = 0
+    for element in self
+      yield element, i
+      i += 1
     end
+    return self
   end
 
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    
+  end
 end
 
 # You will first have to define my_each
@@ -20,12 +25,12 @@ end
 class Array
   # Define my_each here
   def my_each
-    if block_given?
-      for element in self
-        yield element
-      end
-      return self
+    return to_enum(:my_each) unless block_given?
+
+    for element in self
+      yield element
     end
+    return self
   end
 
 end
